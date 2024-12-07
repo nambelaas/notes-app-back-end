45d4cddf397d5a68f3d40ca67fc975d6243bcc97
salmanseif <salman.seif@sirclo.com>
2024-12-07 11:02:32 +0700

    fix bug host value

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const routes = require('./routes');
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+  // eslint-disable-next-line no-undef
+  const env = process.env.NODE_ENV || 'development';
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: env !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
